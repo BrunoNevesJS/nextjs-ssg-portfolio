3bbfabdbc76bee940b7b986c3052181732e4bf56
Bruno Bengtson Neves <brunobn13@hotmail.com>
2022-03-24 16:47:36 -0300

    Refactor next config module export

--- next.config.js
+++ next.config.js
@@ -1,27 +1,4 @@
-/** @type {import('next').NextConfig} */
-const optimizedImages = require('next-optimized-images');
-const withPlugins = require('next-compose-plugins');
-
-const nextConfig = {
-  images: {
-    disableStaticImages: true,
-  },
-  reactStrictMode: true,
+module.exports = {
   basePath: '/nextjs-ssg-portfolio',
   assetPrefix: '/nextjs-ssg-portfolio'
-};
-
-const config = withPlugins(
-  [
-    [
-      optimizedImages,
-      {
-        optimizeImages: false,
-      },
-    ],
-  ],
-  nextConfig
-);
-
-
-module.exports = config
+}
