746a6ce46bb06c5a7c9c4744333215e8f9c51bc6
Bruno Bengtson Neves <brunobn13@hotmail.com>
2022-03-29 18:51:31 -0300

    Mount main wrapper component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components';
+import PropTypes from 'prop-types';
 
 import GlobalStyles from '../styles/global/global'
 import theme from '../styles/theme/theme';
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
 
 export default function MyApp (
   { Component, pageProps }: AppProps): JSX.Element {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import Footer from '../components/footer';
 import Header from '../components/header';
+import Home from '../components/home';
+import Divider from '../components/divider';
 
-export default function Home() {
+export default function Main() {
   return (
     <>
       <Header />
-      
+      <Home />
+      <Divider />
+      <div style={{ backgroundColor: 'white', height: 200}} />
       <Footer />
     </>
   );
