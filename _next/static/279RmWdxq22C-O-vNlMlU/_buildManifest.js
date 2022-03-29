7db363212735fd54b6248a6dc8d651561929f4d1
BrunoNevesJS <BrunoNevesJS@users.noreply.github.com>
2022-03-29 15:05:06 +0000

    Deploying to gh-pages from @ BrunoNevesJS/nextjs-ssg-portfolio@fffc882228d4c8f597300adf83e561ca8f4ada60 🚀

---
+++ _next/static/279RmWdxq22C-O-vNlMlU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-86cc0bb39c1643fd.js","static/chunks/pages/index-9a49b11a67b15d39.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
