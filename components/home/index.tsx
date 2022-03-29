15e0104ed4f1ee89765251acdf4254e867025064
Bruno Bengtson Neves <brunobn13@hotmail.com>
2022-03-29 18:50:56 -0300

    Add wrapper home

---
+++ components/home/index.tsx
@@ -0,0 +1,55 @@
+import { Container, TextContainer, InfosContainer, CodeItem, Curved, Divider, TextItem } from './styles';
+import Animation from '../animation'
+import TextLoop from 'react-text-loop/lib/components/TextLoop';
+
+export default function Home() {
+  const calculateAge = (bornYear: number): number => {
+    return new Date().getFullYear() - bornYear;
+  }
+
+  return (
+    <>
+      <Container data-aos="fade-up">
+        <div>
+          <TextContainer>
+            <TextItem>
+              <h1>Olá</h1>
+              <h2>Me chamo Bruno,</h2>
+              <h2>Sou {""}
+                <TextLoop>
+                  <span className="highlight">Desenvolvedor</span>
+                  <span className="highlight">Programador</span>
+                  <span className="highlight">Full Stack</span>
+                </TextLoop>
+              </h2>
+            </TextItem>
+          </TextContainer>
+          <InfosContainer>
+            <CodeItem data-aos="zoom-in">
+              <span className="comment">//Meu perfil</span>
+              <span className="purple">Profile</span> {'\u007B'}
+              <div>
+                Nome: <span className="blue">Bruno Neves</span>,
+              </div>
+              <div>
+                Idade: <span className="blue">{calculateAge(1997)}</span>,
+              </div>
+              <div>
+                Local: 
+                {'\u007B'}
+                <div>
+                  Cidade: <span className="blue">Belo Horizonte</span>, <br />
+                  Estado: <span className="blue">Minas Gerais</span>, <br />
+                  País: <span className="blue">Brasil</span>,
+                </div>
+                {'\u007D'},
+              </div>
+              {'\u007D'}
+            </CodeItem>
+          </InfosContainer>
+        </div>
+        <Animation path={`${process.env.NEXT_BASE_PATH || ''}/web-developer.json`} />
+      </Container>
+    </>
+  );
+}
