0f144323fa5e72e5d93333b375841cbef446711e
Bruno Bengtson Neves <brunobn13@hotmail.com>
2022-03-29 12:02:36 -0300

    Add main header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
-import Head from 'next/head';
 import Footer from '../components/footer';
-
-const PREFIX = process.env.NEXT_BASE_PATH || ''
-
+import Header from '../components/header';
 
 export default function Home() {
   return (
     <>
-      <main>
-        <h1>Olá Mundo</h1>
-        <Footer />
-      </main>
+      <Header />
+      
+      <Footer />
     </>
   );
 }
